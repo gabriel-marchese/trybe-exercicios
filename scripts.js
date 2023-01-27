b484e40dd84a977cd5b86713279e1a2ec55ccfed
Gabriel Marchese <gabriel.marchese2@gmail.com>
2023-01-27 15:58:24 -0300

    Alguns testes em Js

---
+++ scripts.js
@@ -0,0 +1,53 @@
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for(let index = 0; index < groceryList.length; index +=1){
+//     console.log(groceryList[index]);
+// }
+
+
+
+
+
+
+// let contador = 100 ;
+// let resultado = 0;
+
+// while(contador > 0){
+//     contador -= 5;
+//     resultado += 1;
+// }
+// console.log(resultado);
+
+
+
+
+
+// function sum(a, b) {
+//     a + b;
+//   }
+//   console.log(sum(1, 2));
+
+
+
+
+
+// meuJogo = [6, 8, 16, 30, 44, 59];
+// resultado = [];
+// acertos = 0 ;
+
+
+// for(let indexResult = 1; resultado.length <6; indexResult += 1){
+//   resultado.push(Math.floor((Math.random() * 60) + 1));
+// }
+
+// for(let indexJogo = 0; indexJogo < meuJogo.length; indexJogo +=1){
+//   for(let indexBusca = 0; indexBusca <resultado.length; indexBusca +=1){
+//     if(meuJogo[indexJogo] === resultado[indexBusca]){
+//       acertos += 1;
+//     }
+//   }
+// }
+
+// console.log("Seu Jogo: " + meuJogo);
+// console.log("Resultado: " + resultado);
+// console.log("Você acertou: " + acertos);
